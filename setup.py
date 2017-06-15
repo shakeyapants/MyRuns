8ed2415484fd2be76ffb1995cd8939d4b41165c5
Angelina Nikiforova <nikiforova.angelina@gmail.com>
2017-06-15 21:36:09 +0300

    remove import version from setup

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup
 from os import path
-from MyRuns.version import __version__
 
 here = path.abspath(path.dirname(__file__))
 
@@ -9,7 +8,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
-
+exec(open(path.join(here, 'MyRuns/version.py')).read())
 setup(
     name='MyRuns',
     version=__version__,
